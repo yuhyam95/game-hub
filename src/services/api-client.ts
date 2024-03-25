6cd6eeac2e156afc1950767c02f65f62c387cf37
Yusuf Habu <yuhyam95@gmail.com>
2024-03-25 12:45:56 +0100

    Refactored useGenres

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "1c531601f0484982b5940b6626696580",
   },
 });
